1710900d8e7a4140addf13561072f62e38eeb06e
Arjunchoudhary2027 <studyplace16@gmail.com>
2026-09-04 01:52:08 +0530

    add

--- Day-2/server.js
+++ Day-2/server.js
@@ -2,15 +2,33 @@ const express =require("express")
 const app=express()
 app.use(express.json())
 
-const notes=[]
+const notes=[
+
+]
 
 app.post("/notes",(req,res)=>{
     console.log(req.body)
-    res.send("notes created")
+    notes.push(req.body)
+    console.log(notes)
+    res.send("notes created", notes)
 })
+
+
 app.get("/notes",(req,res)=>{
     res.send(notes)
 })
+
+
+app.delete("/notes/:index",(req,res)=>{
+    delete notes[req.params.index];
+    res.send("Note deleted succesfully", notes);
+})
+
+app.patch("/notes/:index",(req,res)=>{
+    notes[req.params.index].roll=req.body.roll;
+    res.send("Note updated succesfully", notes)
+})
+
 app.listen(3000,()=>{
     console.log("server started");
     
