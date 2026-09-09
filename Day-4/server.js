4e757ec8aa98cc64ec2382744ae948444db736b3
Arjunchoudhary2027 <studyplace16@gmail.com>
2026-09-10 04:33:43 +0530

    Update new add

--- Day-4/server.js
+++ Day-4/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const  connectToDB=require("./src/config/database.js");
 const app=require("./src/app.js");
 
--- Day-4/src/config/database.js
+++ Day-4/src/config/database.js
@@ -2,8 +2,8 @@ const mongoose=require("mongoose");
 const dns = require("dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-function connectToDB(){
-    mongoose.connect("mongodb+srv://arjunkumar09386_db_user:G4EsWPXHFqV5IIcM@back-restart.iisvhld.mongodb.net/Day-4")
+ function connectToDB(){
+      mongoose.connect(process.env.MONGO_URI)
     .then(()=>{
         console.log("connect to mongooDB");
         
